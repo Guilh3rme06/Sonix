c39c86027ed439d508811a7c07b584934c3ea309
Guilh3rme06 <2006guiirodrigues@gmail.com>
2024-11-07 11:23:58 +0000

    Update users and musics

--- users.py
+++ users.py
@@ -18,14 +18,17 @@ connection.commit()
 def insert_user(username, age, favorite_song):
     cursor.execute('INSERT INTO users (username, age, favorite_song) VALUES (?, ?, ?)', (username, age, favorite_song))
     connection.commit()
-    return cursor.fetchall()
+
+
 def find_user(username):
     cursor.execute('SELECT * from users WHERE username = ?', username)
     connection.commit()
     return cursor.fetchall()
+
 def delete_user(username):
     cursor.execute('DELETE FROM users WHERE username = ?', username)
     connection.commit()
+
 def get_all_users():
     cursor.execute('SELECT * FROM users')
     connection.commit()
