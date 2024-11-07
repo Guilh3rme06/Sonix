c305376e4982db7744c60910e9f056a179c88b01
Utilizador <gurodriguesalves12@gmail.com>
2024-11-07 11:37:19 +0000

    x

--- musics.py
+++ musics.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+# Conecta ao banco de dados 'musics.db'
 connection = sqlite3.connect('musics.db')
 cursor = connection.cursor()
 
+# Cria a tabela 'musics' se ela não existir
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS musics(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -14,24 +16,29 @@ cursor.execute('''
     FOREIGN KEY (added_by) REFERENCES users(id)
     )
 ''')
-
 connection.commit()
 
+# Função para inserir uma nova música na tabela 'musics'
 def insert_music(title, artist, year, album, added_by):
     cursor.execute('INSERT INTO musics (title, artist, year, album, added_by) VALUES (?,?,?,?,?)', (title, artist, year, album, added_by))
     connection.commit()
     print('Música inserida com sucesso!')
 
+# Função para obter todas as músicas da tabela 'musics'
 def get_all_songs():
     cursor.execute('SELECT * FROM musics')
     connection.commit()
     return cursor.fetchall()
 
+# Função para remover uma música da tabela 'musics' com base no title
 def delete_song(title):
     cursor.execute('DELETE FROM musics WHERE title = ?', title)
     connection.commit()
     print('Música removida com sucesso!')
 
+# Exclui a tabela 'musics'
 cursor.execute('DROP TABLE musics')
 connection.commit()
+
+# Fecha a conexão com o banco de dados
 connection.close()
--- users.py
+++ users.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+# Conecta ao banco de dados 'users.db'
 connection = sqlite3.connect('users.db')
 cursor = connection.cursor()
 
+# Cria a tabela 'users' se ela não existir
 cursor.execute('''
     CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,28 +14,33 @@ cursor.execute('''
     FOREIGN KEY (favorite_song) REFERENCES musics(id)
         )
     ''')
-
 connection.commit()
 
-def insert_user(username, age, favorite_song):
+# Função para criar um utilizador na tabela 'users'
+def create_user(username, age, favorite_song):
     cursor.execute('INSERT INTO users (username, age, favorite_song) VALUES (?, ?, ?)', (username, age, favorite_song))
     connection.commit()
 
-
+# Função para encontrar um utilizador na tabela 'users'
 def find_user(username):
     cursor.execute('SELECT * from users WHERE username = ?', username)
     connection.commit()
     return cursor.fetchall()
 
+# Função para remover um utilizador da tabela 'users' com base no username
 def delete_user(username):
     cursor.execute('DELETE FROM users WHERE username = ?', username)
     connection.commit()
 
+# Função para obter todos os utilizadores da tabela 'users'
 def get_all_users():
     cursor.execute('SELECT * FROM users')
     connection.commit()
     return cursor.fetchall()
 
+# Exclui a tabela 'users'
 cursor.execute('DROP TABLE users')
 connection.commit()
+
+# Fecha a conexão com o banco de dados
 connection.close()
